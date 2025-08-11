bdc74f61725d92f087232c9e5b4ee19332ea4e3d
piyushpalkeshpanda <piyushpalkeshpanda@gmail.com>
2025-08-11 17:52:44 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+print("Simple Calculator")
+print("Enter two numbers")
+
+#input from users
+
+x = float(input("First Number: "))
+y = float(input("Second Number: "))
+
+#nothing is divisible by 0
+
+print("addition of", x, y, x+y)
+print("subtraction of", x, y, x-y)
+print("multiplication of", x, y, x*y)
+print("division of", x, y, x/y)
